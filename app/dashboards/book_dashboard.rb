4d3e9c77fc7cd628171a82148d731471bcfd673b
Jamie Simper <fromraintohope@gmail.com>
2016-05-08 19:43:02 -0400

    more fixes

--- app/dashboards/book_dashboard.rb
+++ app/dashboards/book_dashboard.rb
@@ -33,9 +33,9 @@ class BookDashboard < Administrate::BaseDashboard
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
     :users,
-    :id
-    :authors
-    :title
+    :id,
+    :authors,
+    :title,
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -34,7 +34,7 @@ class UserDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :user,
+    :name,
     :books,
     :events,
     :id,
